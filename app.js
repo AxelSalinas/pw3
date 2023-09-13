856c17e3607c3fbde87a07dcbb04e560452ba03d
AxelSalinas <72115331+AxelSalinas@users.noreply.github.com>
2023-09-12 21:46:56 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+let x=10;
+let y=2;
+let resultado = x+y;
+console.log(resultado);
+
+//alert("1");
+//alert("2");
+//Alert("3");
+//alert("4");
+
+var numero = 200;
+var numeroDos = 200;
+console.log(numero+numeroDos);
+
+var nombre = prompt("please! give me your name: ");
+console.log(nombre);
+
+//calculadora suma 
+var numero = prompt("please! give a number: ");
+var numeroDos = prompt("please! give a second number: ");
+
+let resultado2 = numero+numeroDos;
+console.log("El resultado es: ", resultado2);
+
+//calculadora multi
+var numero = prompt("please! give a number: ");
+var numeroDos = prompt("please! give a second number: ");
+
+let resultado3 = numero*numeroDos;
+console.log("El resultado es: ", resultado3);
+
+//calculadora div
+var numero = prompt("please! give a number: ");
+var numeroDos = prompt("please! give a second number: ");
+
+let resultado4 = numero/numeroDos;
+console.log("El resultado es: ", resultado4);
